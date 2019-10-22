c2ed7dc98f728b52fea4a47c819b6f5634a2fb06
Marco A Pereira <marcopereirasistemas@gmail.com>
2019-10-22 09:47:35 -0300

    Versao inicial

---
+++ CursoGITHub_NelioAlvez/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CursoGITHub_NelioAlvez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
